251bf533043e9a08a9d3c8fa8315541c32483470
jiangtony <tony.jiang34@gmail.com>
2018-01-08 00:15:39 -0500

    More code cleanup and added comments
    Put repetitive code into new functions. The page loads from the init()
    function.

--- colorgame.js
+++ colorgame.js
@@ -6,37 +6,15 @@ var winnerHeader = document.getElementById("winningcolor");
 var resetButton = document.querySelector("#resetBtn");
 var easyButton = document.querySelector("#easyBtn");
 var hardButton = document.querySelector("#hardBtn");
+var modeButtons = document.querySelectorAll(".modeBtns");
 var hardMode = true;
 
 init();
 
 function init() {
-	hardButton.classList.add("selected");
+	modeButtons[1].classList.add("selected");
 	resetButton.addEventListener("click", reset);
-
-	easyButton.addEventListener("click", function() {
-		this.classList.add("selected");
-		hardButton.classList.remove("selected");
-		hardMode = false;
-		// Hide bottom 3 squares
-		for (var i = 3; i < squares.length; i++) {
-			squares[i].style.display = "none";
-		}
-		reset();
-	});
-
-	hardButton.addEventListener("click", function() {
-		this.classList.add("selected");
-		easyButton.classList.remove("selected");
-		hardMode = true;
-		// Reenable bottom 3 squares
-		for (var i = 3; i < squares.length; i++) {
-			squares[i].style.display = "block";
-		}
-		reset();
-	});
-
-
+	modeButtonSetup();
 	reset();
 	initializeSquareListeners();
 }
@@ -50,6 +28,36 @@ function newWinningColor() {
 	}
 }
 
+// Add listeners to Easy/Hard buttons
+function modeButtonSetup() {
+	for (var i = 0; i < modeButtons.length; i++) {
+		modeButtons[i].addEventListener("click", function() {
+			modeButtons[0].classList.remove("selected");
+			modeButtons[1].classList.remove("selected");
+			this.classList.add("selected");
+			// set hardMode to true or false
+			this.textContent === "EASY" ? hardMode = false : hardMode = true;
+			changeModes();
+			reset();
+		});
+	}
+}
+
+// Helper for modeButtonSetup()
+// Enable/Reenable bottom 3 squares based on mode
+function changeModes() {
+	if (hardMode) {
+		for (var i = 3; i < squares.length; i++) {
+			squares[i].style.display = "block";
+		}
+	} else {
+		for (var i = 3; i < squares.length; i++) {
+			squares[i].style.display = "none";
+		}
+	}
+}
+
+// Resets the game
 function reset() {
 	winningIndex = newWinningColor();
 	// Generate new colors and set each square
@@ -57,29 +65,28 @@ function reset() {
 		// Add color to each square
 		var color = generateRandomColor();
 		squares[i].style.backgroundColor = color;
-
-		// Set the winning color
+		// The winning index was previously chosen by newWinningColor()
+		// Sets winningColor equal to the color at this index
 		if (i == winningIndex) {
 			winningColor = color;
 		}
 	}
-
 	// Change the header color and text back to previous values
 	document.getElementById("header").style.backgroundColor = "steelblue";
 	messageDisplay.textContent = "";
-
 	winnerHeader.textContent = winningColor; // Display RGB string in header
 	resetButton.textContent = "NEW COLORS";
 }
 
+// Generates random RGB values from 0-255
 function generateRandomColor() {
 	var r,g,b; 
 	do {
 		r = Math.floor(Math.random() * 256);
 		g = Math.floor(Math.random() * 256);
 		b = Math.floor(Math.random() * 256);
-	} 
-	while ( r === 33 && g === 33 && b === 33); // Ensure that the square will not be same color as background
+	} // Ensure that the square will not be same color as background
+	while ( r === 33 && g === 33 && b === 33);
 	return "rgb(" + r + ", " + g + ", " + b + ")";
 }
 
@@ -98,6 +105,7 @@ function initializeSquareListeners() {
 	}
 }
 
+// Changes the rest of the squares and header color when correct guess
 function winner() {
 	messageDisplay.textContent = "Correct!";
 	for (var i = 0; i < squares.length; i++) {
@@ -105,4 +113,3 @@ function winner() {
 	}
 	document.getElementById("header").style.backgroundColor = winningColor;
 }
-
