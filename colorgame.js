2b47b7a7d5eac8d23c4a6f5ba60b9c83462b8bd3
jiangtony <tony.jiang34@gmail.com>
2018-01-07 19:20:28 -0500

    Added easy/hard mode buttons
    Easy and hard mode buttons do not work yet

--- colorgame.js
+++ colorgame.js
@@ -4,38 +4,92 @@ var winningColor; // The RGB string that will be displayed at the top
 
 var winningIndex = newWinningColor();
 
+var messageDisplay = document.getElementById("resultmessage");
+
+var winnerHeader = document.getElementById("winningcolor");
+
+var resetButton = document.querySelector("#resetBtn");
+var easyButton = document.querySelector("#easyBtn");
+var hardButton = document.querySelector("#hardBtn");
+
+hardButton.classList.add("selected");
+
+resetButton.addEventListener("click", reset);
+easyButton.addEventListener("click", function() {
+	this.classList.add("selected");
+	hardButton.classList.remove("selected");
+
+	// remove listeners from bottom 3 and hide them
+	
+});
+
+hardButton.addEventListener("click", function() {
+	this.classList.add("selected");
+	easyButton.classList.remove("selected");
+
+	//add listeners back to bottom 3
+	
+});
+
+
+reset();
+
+// Add listeners to the squares
+for (var i = 0; i < squares.length; i++) {
+	squares[i].addEventListener("click", function() {
+		if (this.style.backgroundColor === winningColor) {
+			winner();
+			resetButton.textContent = "PLAY AGAIN?";
+		} else {
+			//alert("Wrong");
+			this.style.backgroundColor = "#333333";
+			messageDisplay.textContent = "Try Again!";
+		}
+	});
+}
+
 // Generate number from 0-5. That number will decide which square is the winner.
 function newWinningColor() {
 	return Math.floor(Math.random() * 6);
 }
 
-function randomColor() {
-	var r = Math.floor(Math.random() * 256);
-	var g = Math.floor(Math.random() * 256);
-	var b = Math.floor(Math.random() * 256);
+function generateRandomColor() {
+	var r,g,b; 
+	do {
+	r = Math.floor(Math.random() * 256);
+	g = Math.floor(Math.random() * 256);
+	b = Math.floor(Math.random() * 256);
+	} 
+	while ( r == 33 && g == 33 && b == 33); // Ensure that the square will not be same color as background
+	// or just put a border around each square?
 	return "rgb(" + r + ", " + g + ", " + b + ")";
 }
 
-for (var i = 0; i < squares.length; i++) {
-	// Add color to each square
-	var color = randomColor();
-	squares[i].style.backgroundColor = color;
-
-	// Set the winning color
-	if (i == winningIndex) {
-		winningColor = color;
+function winner() {
+	messageDisplay.textContent = "Correct!";
+	for (var i = 0; i < squares.length; i++) {
+		squares[i].style.backgroundColor = winningColor;
 	}
-
-	// Add listeners to the square
-	squares[i].addEventListener("click", function() {
-		if (this.style.backgroundColor === winningColor) {
-			//alert("Correct");
-		} else {
-			//alert("Wrong");
-			this.style.backgroundColor = "#333333";
-		}
-	});
+	document.getElementById("header").style.backgroundColor = winningColor;
 }
 
-var winnerHeader = document.getElementById("winningcolor");
-winnerHeader.textContent = winningColor;
+function reset() {
+	// Generate new colors and set each square
+	for (var i = 0; i < squares.length; i++) {
+		// Add color to each square
+		var color = generateRandomColor();
+		squares[i].style.backgroundColor = color;
+
+		// Set the winning color
+		if (i == winningIndex) {
+			winningColor = color;
+		}
+	}
+	// Change the header color back
+	document.getElementById("header").style.backgroundColor = "#333333";
+	messageDisplay.textContent = "Pick a Color";
+
+	winnerHeader.textContent = winningColor; // Display RGB string in header
+	resetButton.textContent = "New Colors";
+
+}
